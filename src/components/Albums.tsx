a38d47a4632b1fd27209b0e0aa5563fe4c485fb5
KhrustDanil <danil.khrust29@gmail.com>
2024-08-21 20:25:31 +0300

    fix import

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useAlbums } from "../context/AlbumsContext";
+import { useAlbums } from "../context/useAlbums";
 
 const Albums = () => {
   const { albums } = useAlbums();
